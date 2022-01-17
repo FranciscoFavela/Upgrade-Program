68705b628be4d6400877532bd58553c471a34157
Francisco Javier Favela Najera <francisco211maco@gmail.com>
2022-01-17 14:29:50 -0600

    v1 generics

---
+++ src/main/java/com/example/persona/services/BaseServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.persona.services;
+
+import com.example.persona.entities.Base;
+
+import java.io.Serializable;
+
+public class BaseServiceImpl<E extends Base, ID extends Serializable> implements BaseService<E, ID> {
+}
